92286b38afd43db88b4cabd445cda64be0b375cf
zj <a6377248329@qq.com>
2019-04-09 17:50:11 +0800

    改变n次

--- Springmvc/src/com/Controller/springmvcController.java
+++ Springmvc/src/com/Controller/springmvcController.java
@@ -18,18 +18,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.Poj.Student;
 
-//接口/类      注解      配置
-@SessionAttributes("student") // 如果要在request中存放student对象,则将该对象同时放在session域中我在改变第三次
-@Controller // 代表这个类是一个控制器改变了
+//没有改变吗
+@SessionAttributes("student") // 濡傛灉瑕佸湪request涓瓨鏀緎tudent瀵硅薄,鍒欏皢璇ュ璞″悓鏃舵斁鍦╯ession鍩熶腑鎴戝湪鏀瑰彉绗笁娆�
+@Controller // 浠ｈ〃杩欎釜绫绘槸涓�涓帶鍒跺櫒鏀瑰彉浜�
 public class springmvcController {
 	@RequestMapping("welcome")
 	public String welcome() {
-		return "success"; // 这里会拼接前缀和后缀,默认使用的是请求转发
+		return "success"; // 杩欓噷浼氭嫾鎺ュ墠缂�鍜屽悗缂�,榛樿浣跨敤鐨勬槸璇锋眰杞彂
 	}
 
 	@RequestMapping("testModelAndView")
-	public ModelAndView testModelAndView() { // 返回既有数据又有视图
-		ModelAndView mv = new ModelAndView("testModelAndView");// 仍然会加前缀和后缀(V)
+	public ModelAndView testModelAndView() { // 杩斿洖鏃㈡湁鏁版嵁鍙堟湁瑙嗗浘
+		ModelAndView mv = new ModelAndView("testModelAndView");// 浠嶇劧浼氬姞鍓嶇紑鍜屽悗缂�(V)
 		Student stu1 = new Student();
 		stu1.setId(1);
 		stu1.setName("zj");
@@ -45,7 +45,7 @@ public class springmvcController {
 		stu1.setName("zj");
 		stu1.setAge("22");
 		mm.put("student", stu1);
-		return "redirect:/views/success.jsp";// 使用重定向,需要写完整的前缀和后缀
+		return "redirect:/views/success.jsp";// 浣跨敤閲嶅畾鍚�,闇�瑕佸啓瀹屾暣鐨勫墠缂�鍜屽悗缂�
 	}
 
 	@RequestMapping("testMap")
@@ -91,18 +91,18 @@ public class springmvcController {
 	}
 
 	@RequestMapping("testUpload")
-	public String testUpload(@RequestParam("file") MultipartFile file) throws IOException {// 前端传的数据注入进去
-		InputStream input = file.getInputStream();// 得到文件的输入流
-		String filename = file.getOriginalFilename();// 得到原始文件名
-		OutputStream output = new FileOutputStream("d:\\" + filename);// 将文件上传到指定目录
+	public String testUpload(@RequestParam("file") MultipartFile file) throws IOException {// 鍓嶇浼犵殑鏁版嵁娉ㄥ叆杩涘幓
+		InputStream input = file.getInputStream();// 寰楀埌鏂囦欢鐨勮緭鍏ユ祦
+		String filename = file.getOriginalFilename();// 寰楀埌鍘熷鏂囦欢鍚�
+		OutputStream output = new FileOutputStream("d:\\" + filename);// 灏嗘枃浠朵笂浼犲埌鎸囧畾鐩綍
 
 		byte[] bs = new byte[1024];
 		int len = -1;
-		while ((len = input.read(bs)) != -1) {// 从输入流里面读取1024个字节到缓冲区,len=-1表示没有东西可以读取
-			output.write(bs, 0, len); // 将缓冲区的数据不停读到目的地
+		while ((len = input.read(bs)) != -1) {// 浠庤緭鍏ユ祦閲岄潰璇诲彇1024涓瓧鑺傚埌缂撳啿鍖�,len=-1琛ㄧず娌℃湁涓滆タ鍙互璇诲彇
+			output.write(bs, 0, len); // 灏嗙紦鍐插尯鐨勬暟鎹笉鍋滆鍒扮洰鐨勫湴
 		}
 		output.close();
-		System.out.println("上传成功");
+		System.out.println("涓婁紶鎴愬姛");
 		return "success";
 	}
 
